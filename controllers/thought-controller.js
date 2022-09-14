895b016e66d9fc7e18fd50f4e312664e24effe3b
diegorodd <dieegoa@icloud.com>
2022-09-13 22:40:08 -0400

    finshed up withe reaction section

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -71,3 +71,32 @@ deleteThought({ params }, res) {
         })
         .catch(err => res.status(400).json(err));
 },
+ // Create a reaction stored in a single thought's array
+ addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $push: { reactions: body } },
+        { new: true, runValidators: true }
+    )
+    .then(dbThoughtData => {
+        if (!dbThoughtData) {
+            res.status(404).json({ message: 'No thought found with this ID!' });
+            return;
+        }
+        res.json(dbThoughtData);
+    })
+    .catch(err => res.json(err));
+},
+// Delete a reaction by the reactionId
+deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $pull: { reactions: { reactionId: params.reactionId } } },
+        { new: true }
+    )
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => res.json(err));
+}
+};
+
+module.exports = thoughtController;
