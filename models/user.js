2a295ab7f097492f69cad6ec0aaef5f067efe6a3
diegorodd <dieegoa@icloud.com>
2022-09-13 23:10:55 -0400

    started with user.js and adding the username&email

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
