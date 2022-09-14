4e1a4cd61df2d5f5477e783e223b3d549b690cc6
diegorodd <dieegoa@icloud.com>
2022-09-13 22:35:10 -0400

    started withe controllers thoughts

---
+++ controllers/thought-controller.js
@@ -0,0 +1,30 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+
+  getThoughts(req, res) {
+    Thought.find()
+      .sort({ createdAt: -1 })
+      .then((dbThoughtData) => {
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Get a single thought by id
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          return res.status(404).json({ message: 'Thought with this ID does not exist.' });
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
