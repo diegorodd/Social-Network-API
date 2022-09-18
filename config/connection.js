a2b23187a718fdab6b979da200fe3ed6939adc1e
diegorodd <dieegoa@icloud.com>
2022-09-18 13:34:37 -0400

    mongodb

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Social-Network-API', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Social-Network-API', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
